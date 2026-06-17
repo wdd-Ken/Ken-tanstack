98d8acf07de364c75e0735b9060afbd803b318bf
wdd-Ken <178758721+wdd-Ken@users.noreply.github.com>
2026-06-17 18:30:15 +0800

    2026.06.17 form update

--- f2e/src/routes/form/index.tsx
+++ f2e/src/routes/form/index.tsx
@@ -1,38 +1,68 @@
 import { cn } from '#/lib/utils'
 import { createFileRoute } from '@tanstack/react-router'
-import { useEffect, useState } from 'react'
+import { useId, useRef } from 'react'
+import { useState } from 'react'
 
 export const Route = createFileRoute('/form/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    gender: ''
-  })
+  const [preview, setPreview] = useState('')
+  const [step, setStep] = useState(1)
+  const textareaId = useId()
+  const formRef = useRef<HTMLFormElement>(null)
 
-  const handleChange = ( e: React.ChangeEvent<HTMLInputElement> ) => {
-      const { name, value } = e.target
+  const goNext = () => {
+    const currentStep = document.querySelector(
+      `[data-step="${step}"]`
+    )
 
-      setFormData((prev) => ({
-        ...prev,
-        [name]: value,
-      }))
+    const fields = currentStep?.querySelectorAll(
+      'input, textarea, select'
+    ) as NodeListOf<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    > | undefined
+
+    const isValid = [...fields ?? []].every(field =>
+      field.checkValidity()
+    )
+
+    if (!isValid) {
+      const invalidField = currentStep?.querySelector(
+        ':invalid'
+      ) as HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement | null
+
+      invalidField?.reportValidity()
+
+      return
     }
 
-  const handleChangeCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData((prev) => ({
-      ...prev,
-      gender: e.target.checked ? '男' : '女'
-    }))
+    setStep(prev => prev + 1)
+  }
+  
+  const goPrev = () => setStep(prev => prev - 1)
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = e.target.files?.[0]
+
+    if (!file) return
+    console.log(file)
+    setPreview(URL.createObjectURL(file))
   }
 
-  useEffect(() => {
-    console.log('formData',formData)
-  }, [formData])
+  const handleSubmit = (e: {
+    target: HTMLFormElement | undefined ; 
+    preventDefault: () => void 
+  }) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.target)
+    const formJson = Object.fromEntries(formData.entries())
+    console.log(formJson)
+  }
   
   return (
     <>
@@ -42,81 +72,198 @@ function RouteComponent() {
         'w-3xl',
         'm-auto',
         'mt-8',
-        // 'absolute',
-        // 'top-[50%]',
-        // 'left-[50%]',
-        // 'min-w-150',
         'bg-white',
         'text-black',
         'rounded-sm',
-        // 'translate-[-50%]',
       )}>
-        <form className={cn(
-          'flex',
-          'flex-col',
-          'w-full',
-          'gap-2.5'
-        )}>
-          <div>Name:</div>
-          <input
-            name="name"
-            // value={formData.name}
-            // onChange={handleChange}
+        <form 
+          onSubmit={handleSubmit}
+          className={cn(
+            'flex',
+            'flex-col',
+            'w-full',
+            'gap-2.5'
+          )}
+          ref={formRef}
+          >
+          <div 
+            data-step="1"
+            style={{ display: step === 1 ? 'block' : 'none' }}
             className={cn(
-              'p-1',
-              'border',
-              'rounded-xs',
-              'text-sm',
-              'min-h-[34px]'
+              'flex',
+              'flex-col',
+              'w-full',
+              'gap-2.5'
             )}
-            defaultValue={'ffff'}
+          >
+            <div>Name:</div>
+            <input
+              name="name"
+              defaultValue=''
+              required
+              className={cn(
+                'p-1',
+                'border',
+                'rounded-xs',
+                'text-sm',
+                'min-h-8.5'
+              )}
             />
-          <div>Email:</div>
-          <input
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className={cn(
-              'p-1',
-              'border',
-              'rounded-xs',
-              'text-sm',
-              'min-h-[34px]'
-            )}
+            <div>Email:</div>
+            <input
+              name="email"
+              defaultValue=''
+              required
+              className={cn(
+                'p-1',
+                'border',
+                'rounded-xs',
+                'text-sm',
+                'min-h-8.5'
+              )}
             />
-          <div>Phone:</div>
-          <input
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
+            <div>Phone:</div>
+            <input
+              name="phone"
+              defaultValue=''
+              required
+              className={cn(
+                'p-1',
+                'border',
+                'rounded-xs',
+                'text-sm',
+                'min-h-8.5'
+              )}
+            />
+            <label>
+              <input 
+                type="checkbox" 
+                name='checkbox'
+                value="checkbox1"
+                defaultChecked={true}
+                required
+              />
+              checkbox1
+            </label>
+            <label>
+              <input 
+                type="checkbox" 
+                name='checkbox2'
+                value="checkbox2"
+                defaultChecked={false}
+                required
+              />
+              checkbox2
+            </label>
+            <label>
+              <input 
+                type="checkbox" 
+                name='checkbox3'
+                value="checkbox3"
+                defaultChecked={true}
+                required
+              />
+              checkbox3
+            </label>  
+            <button onClick={goNext}>next</button>      
+          </div>
+          <div
+            data-step="2"
+            style={{ display: step === 2 ? 'block' : 'none' }}
             className={cn(
-              'p-1',
-              'border',
-              'rounded-xs',
-              'text-sm',
-              'min-h-[34px]'
+              'flex',
+              'flex-col',
+              'w-full',
+              'gap-2.5'
             )}
-          />
-          <div>
+          >
+            <div>
+              <label htmlFor={textareaId}>Content</label>
+              <textarea 
+                name="postContent" 
+                id={textareaId}
+                defaultValue='default content'
+                required
+                className={cn(
+                  'border',
+                )}
+              />
+            </div>
+            <div>
+              <label>
+                選擇一個水果:
+                <select 
+                  name="水果" 
+                  required
+                  defaultValue="香蕉"
+                >
+                  <option value="蘋果">蘋果</option>
+                  <option value="香蕉">香蕉</option>
+                  <option value="橘子">橘子</option>
+                </select>
+              </label>
+            </div>
+            <button onClick={goPrev}>prev</button>
+            <button onClick={goNext}>next</button>
+          </div>
+          <div
+            data-step="3"
+            style={{ display: step === 3 ? 'block' : 'none' }}
+            className={cn(
+              'flex',
+              'flex-col',
+              'w-full',
+              'gap-2.5'
+            )}
+          >
+            <label htmlFor='file'>Content</label>
             <input 
-              type="checkbox" 
-              id='male'
-              checked={formData.gender === '男'}
-              onChange={handleChangeCheckbox}
+              type="file" 
+              id='file' 
+              onChange={handleChange}
+              required
             />
-            <label htmlFor="male">男性</label>
+            {preview && (
+              <div className={cn(
+                'w-80',
+              )}>
+                <img
+                  src={preview}
+                  alt=""
+                  className={cn(
+                    'w-full',
+                    'h-full',
+                    'object-contain',
+                  )}
+                />
+              </div>
+            )}
+            <label>
+              <input
+                type="radio"
+                name="gender"
+                value="male"
+                defaultChecked
+                required
+              />
+              男
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="gender"
+                value="female"
+              />
+              女
+            </label>
+            <button onClick={goPrev}>prev</button>
+            <button 
+              type="submit"
+              className={cn(
+                'cursor-pointer',
+              )} 
+            >送出</button>
           </div>
-          <div>
-            <input type="checkbox" id='female'/>
-            <label htmlFor="female">女性</label>
-          </div>
-          <div></div>
-          <button 
-            // type="submit"
-            className={cn(
-              'cursor-pointer',
-            )} 
-          >送出</button>
         </form>
       </div>
     </>
